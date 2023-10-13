3fda34a84ff18f1b23b6e567aefcda377c0622f7
Ziad Alfadl <alfadlziad@gmail.com>
2023-10-13 17:13:21 -0400

    Add to About and Project

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,18 +1,15 @@
 import charcreate from "./creatornator.png";
 import jate from "./jate.png";
-import spotifyList from "./spotifylist.png";
 import poemApp from "./poemapp.png";
-import profileDesign from "./profiledesign.png";
 import readmeGenerator from "./readmegenerator.png";
 import avatar from "./avatar.jpg";
-
+import SMAPI from "./SMAPI.png";
 //central file to import/export all images
 export {
   charcreate,
   jate,
-  spotifyList,
   poemApp,
-  profileDesign,
   readmeGenerator,
   avatar,
+  SMAPI,
 };
